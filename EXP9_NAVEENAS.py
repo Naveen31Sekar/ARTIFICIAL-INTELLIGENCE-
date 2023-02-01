37cb4fe9c36912389cea4573da432f6632b32dab
Naveen31Sekar <89571113+Naveen31Sekar@users.noreply.github.com>
2023-02-01 13:34:28 +0530

    Update EXP9_NAVEENAS.py

--- EXP9_NAVEENAS.py
+++ EXP9_NAVEENAS.py
@@ -1,10 +1,8 @@
-punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-
-my_string = input() 
-
-my_string = input("Enter a string: ")
-no_punct = ""
-for char in my_string:
-   if char not in punctuations:
-       no_punct = no_punct + char
-print(no_punct)
+p = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+string = input() 
+string = input("Enter a string: ")
+opstring= ""
+for i in my_string:
+   if i not in punctuations:
+       opstring = opstring+ i
+print(opstring)
